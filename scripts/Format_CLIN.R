266b7d5a7110eaaa447d8549c5f9d835c50fc778
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-08 17:00:38 -0400

    update clin processing

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -3,15 +3,18 @@ input_dir <- args[1]
 output_dir <- args[2]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 
-clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
+clin_original = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
 
-clin = cbind( clin[ , c( "Best.Confirmed.Overall.Response","Sex","ANONPT_ID","os","censOS","Tissue" ) ] , "PD-1/PD-L1" , NA , NA , NA , NA , NA , NA , NA , NA , NA )
+selected_cols <- c( "Best.Confirmed.Overall.Response","Sex","ANONPT_ID","os","censOS","Tissue" )
+clin = cbind( clin_original[ , selected_cols  ] , "PD-1/PD-L1" , NA , NA , NA , NA , NA , NA , NA , NA , NA )
 colnames(clin) = c( "recist" , "sex" , "patient" , "t.os"  ,"os" , "primary" , "drug_type" , "age" , "t.pfs", "pfs"  , "histo" , "stage" , "dna" , "rna" , "response.other.info" , "response" )
 
 clin$recist[ clin$recist %in% "NE"] = NA
 clin$response = Get_Response( data=clin )
 clin$patient = paste( "P" , clin$patient , sep="" )
+clin_original$ANONPT_ID <- paste( "P" , clin_original$ANONPT_ID , sep="" )
 
 case = read.csv( file.path(output_dir, "cased_sequenced.csv"), stringsAsFactors=FALSE , sep=";" )
 clin$rna[ clin$patient %in% case[ case$expr %in% 1 , ]$patient ] = "tpm"
@@ -29,6 +32,8 @@ clin$primary[ is.na(clin$primary) ] = "Unknown"
 
 clin = clin[ , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
 clin$primary[ clin$primary %in% "Ureter" ] = "Ureteral"
+
+clin <- format_clin_data(clin_original, 'ANONPT_ID', selected_cols, clin)
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
 
